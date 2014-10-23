89f9af240a4015916aca0a1e1e5ba5224be58392
umeyuki <umeyuki1326+github@gmail.com>
2014-10-23 17:37:15 +0900

    正常系テスト green

--- spec/missingnumer_spec.rb
+++ spec/missingnumer_spec.rb
@@ -1,44 +1,57 @@
 require 'spec_helper'
-
+require 'pry'
 describe MissingNumber do
 
   describe :parse do
     
     context 'String argument' do
-
       before(:each) do
-        @numbers = [1..10000].shuffle
+        @numbers = (1..10000).to_a.shuffle
       end
 
-      it 'should be 1' do
-        expected = @numbers.delete(1)
-        expect(MissingNumber.parse(@numbers)).to eq(expected)
+      context 'Success' do
+        it 'should be 2' do
+          expected = @numbers.delete(2)
+          expect(MissingNumber.parse(@numbers.to_s)).to eq(expected)
+        end
+
+        it 'should be 12' do
+          expected = @numbers.delete(12)
+          expect(MissingNumber.parse(@numbers.to_s)).to eq(expected)
+        end
+
+        it 'should be 123' do
+          expected = @numbers.delete(123)
+          expect(MissingNumber.parse(@numbers.to_s)).to eq(expected)
+        end
+
+        it 'should be 1234' do
+          expected = @numbers.delete(1234)
+          expect(MissingNumber.parse(@numbers.to_s)).to eq(expected)
+        end
+
+        it 'should be 10000' do
+          expected = @numbers.delete(1234)
+          expect(MissingNumber.parse(@numbers.to_s)).to eq(expected)
+        end
       end
 
-      it 'should be 12' do
-        expected = @numbers.delete(12)
-        expect(MissingNumber.parse(@numbers)).to eq(expected)
-      end
+      # context 'Error' do
 
-      it 'should be 123' do
-        expected = @numbers.delete(123)
-        expect(MissingNumber.parse(@numbers)).to eq(expected)
-      end
+      #   it 'should return size error' do
+      #     @numbers = (1..9999).to_a.shuffle
+      #     expect{MissingNumber.parse(@numbers)}.to raise_error("Number should be 9,999 elements")  
+      #   end
 
-      it 'should be 1234' do
-        expected = @numbers.delete(1234)
-        expect(MissingNumber.parse(@numbers)).to eq(expected)
-      end
-
-      it 'should be 10000' do
-        expected = @numbers.delete(1234)
-        expect(MissingNumber.parse(@numbers)).to eq(expected)
-      end
-
-      # it 'should return ' do
-      #   @numbers = (1..9999).to_a.shuffle
-      #   expect{get_progress_name(input)}  
+      #   it 'should return type error' do
+      #     @numbers = (1..9999).to_a.shuffle
+      #     @numbers.delete(1234)
+      #     @numbers.push("1234")
+      #     expect{MissingNumber.parse(@numbers)}.to raise_error("Numbers should be Fixnum")  
+      #   end
+        
       # end
+
       
     end
 
