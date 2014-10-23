1624c44c48ac882ecce28c4cebde212cd06072d5
umeyuki <umeyuki1326+github@gmail.com>
2014-10-23 19:05:17 +0900

    remove ignore_number method

--- lib/missingnumber.rb
+++ lib/missingnumber.rb
@@ -16,26 +16,22 @@ module MissingNumber
   def parse_string(string)
     numbers = eval string
     validate(numbers)
-    ignore_number(numbers)
+    @@total_sum - numbers.inject(:+)
   end
   
   def parse_io(io)
     parse_string(io.gets)
   end
   
-  def ignore_number(numbers)
-    @@total_sum - numbers.inject(:+)
-  end
-
   def validate(numbers)
     raise "Number should be 9,999 elements" if numbers.size != 9999
     raise "Numbers should be Fixnum" if numbers.find_all {|num| num.class != Fixnum }.any?
   end
   
-  module_function :parse,:parse_io, :parse_string, :validate, :ignore_number
+  module_function :parse,:parse_io, :parse_string, :validate
   
   class <<self
-    private :parse_io, :parse_string, :validate, :ignore_number
+    private :parse_io, :parse_string, :validate 
   end
   
 end
